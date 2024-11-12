24249171c0c4e70e2eecf25d0be1ce5b6a884e5d
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-11-12 14:57:34 +0600

    added password validation

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,21 +1,46 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../Firebase.init";
+import { useState } from "react";
 
 
 const Register = () => {
+  const [success, setSuccess] = useState(false)
+  const [errorMessage , setErrorMessage] = useState('')
+
+
+
 
     const handleRegister = (event)=>{
         event.preventDefault()
         const email = event.target.email.value;
         const password = event.target.password.value;
+        
+        // reset error status 
+        setErrorMessage('')
+        setSuccess(false)
+
+        if(password.length < 6){
+          setErrorMessage('Password should in 6 Characters')
+          return;
+        }
+
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+        if (!passwordRegex.test(password)) {
+            setErrorMessage('Password should contain At least one uppercase, one lowercase, one number, one special character');
+            return;
+        }
 
         console.log(email,password)
         createUserWithEmailAndPassword(auth, email, password)
         .then(result => {
             console.log(result.user)
+            setSuccess(true)
         })
         .catch(error => {
-            console.log(error)
+            console.log(error.message)
+            setErrorMessage(error.message)
+            setSuccess(false)
         })
 
     }
@@ -39,6 +64,7 @@ const Register = () => {
               className="input input-bordered"
               
             />
+
           </div>
           <div className="form-control">
             <label className="label">
@@ -51,11 +77,20 @@ const Register = () => {
               className="input input-bordered"
               
             />
+
           </div>
           <div className="form-control mt-6">
             <button className="btn btn-primary">Login</button>
           </div>
         </form>
+        {
+          errorMessage && 
+          <p className="text-red-600 text-center mb-2 font-bold px-1">{errorMessage}</p>
+        }
+        {
+          success && 
+          <p className="text-green-600 text-center mb-2 font-bold">Sign up successfull</p>
+        }
       </div>
     </div>
   );
